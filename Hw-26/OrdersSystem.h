1b243236b6301c5abc2937c032e1d8429c71a416
sirossasa7@gmail.com <sirossasa7@gmail.com>
2026-05-09 22:52:40 +0200

    OrdersSysten.h

---
+++ Hw-26/OrdersSystem.h
@@ -0,0 +1,13 @@
+#include "Order.h"
+#include <vector>
+
+class OrdersSystem {
+private:
+    std::vector<Order> orders;
+
+public:
+    void addOrder(const Order& order);
+    void sortOrders();
+    void processNextOrder();
+    void displayAll() const;
+};
